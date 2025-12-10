4255c599759226203ed42c9d4d4c964ce0528474
Caden Kim <hyperdx1@gmail.com>
2025-12-10 21:09:33 +0900

    Edit py script

--- single_sample_GC_count.py
+++ single_sample_GC_count.py
@@ -19,27 +19,32 @@ bin_locations = pd.read_csv(bin_location_file).values.tolist()
 lag = 10
 final_GC_array = np.zeros((end_len-start_len+1, 101))
 
+command = 'samtools view -H {bamPath} | grep @SQ | grep chr'.format(bamPath=input_bam)
+p = subprocess.run(command, shell=True, capture_output=True, text=True)
+BAM_FLAG = len(p.stdout)
+
+reference_genome = pysam.FastaFile(reference_genome_path)
+all_ref_contigs = reference_genome.references
+REF_FLAG = 0
+if 'chr' in all_ref_contigs[0]:
+    REF_FLAG = 1
+
+
 
 def count_GC(sub_bin_locations):
     global reference_genome_path, input_bam, start_len, end_len
     GC_array = np.zeros((end_len-start_len+1, 101))
     pybam = pysam.AlignmentFile(input_bam, "rb")
-    command = 'samtools view -H {bamPath} | grep @SQ | grep chr'.format(bamPath=input_bam)
-    p = subprocess.run(command, shell=True, capture_output=True, text=True)
-    bam_flag = len(p.stdout)
+    
 
     reference_genome = pysam.FastaFile(reference_genome_path)
-    all_ref_contigs = reference_genome.references
-    ref_flag = 0
-    if 'chr' in all_ref_contigs[0]:
-        ref_flag = 1
 
     for bin_location in sub_bin_locations:
         contig, start, end = bin_location[0], int(bin_location[1]), int(bin_location[2])
         ref_contig = contig
-        if ref_flag==0: # checks whether ref contig has 'chr' or not
+        if REF_FLAG==0: # checks whether ref contig has 'chr' or not
             ref_contig = ref_contig[3:]
-        if bam_flag==0: # checks whether bam contig has 'chr' or not
+        if BAM_FLAG==0: # checks whether bam contig has 'chr' or not
             contig = contig[3:]
         for read in pybam.fetch(contig, start, end): # window
             if (read.flag & 1024 == 0) and (read.flag & 2048 == 0) and (read.flag & 4 == 0) and (read.flag & 8 == 0):
@@ -50,8 +55,12 @@ def count_GC(sub_bin_locations):
                         ref_end = read.reference_start + length - lag
                         ref_seq = reference_genome.fetch(ref_contig, ref_start, ref_end)
                         if len(ref_seq)!=0:
-                            GC_cnt = ref_seq.count('G') + ref_seq.count('C') + ref_seq.count('g') + ref_seq.count('c')
-                            AT_cnt = ref_seq.count('A') + ref_seq.count('T') + ref_seq.count('a') + ref_seq.count('t')
+                            u_seq = ref_seq.upper()
+                            GC_cnt = u_seq.count('G') + u_seq.count('C')
+                            AT_cnt = u_seq.count('A') + u_seq.count('T')
+
+                            # GC_cnt = ref_seq.count('G') + ref_seq.count('C') + ref_seq.count('g') + ref_seq.count('c')
+                            # AT_cnt = ref_seq.count('A') + ref_seq.count('T') + ref_seq.count('a') + ref_seq.count('t')
                             total_cnt = GC_cnt + AT_cnt
                             valid_percent = float( total_cnt/len(ref_seq) )
                             if valid_percent>=0.9:
